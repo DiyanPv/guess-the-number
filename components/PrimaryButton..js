13a8d5a969df53d99d3699bcee3659349f2df4a4
unknown <diyanpetrov61@gmail.com>
2023-12-01 18:45:52 +0000

    buttons added and initial styling

--- components/PrimaryButton..js
+++ components/PrimaryButton..js
@@ -1,12 +1,41 @@
-import { View, Text } from 'react-native'
+import { View, Text, Pressable, StyleSheet } from 'react-native'
 import React from 'react'
 
 const PrimaryButton = ({ children }) => {
+    const pressHandler = () => {
+        console.log(`Pressed!`)
+    }
+
     return (
-        <View>
-            <Text>{children}</Text>
+        <View style={styles.buttonOuterContainer}>
+            <Pressable onPress={pressHandler} android_ripple={{ color: `#690237` }} style={({pressed}) => pressed ? [styles.buttonInnerContainer, styles.pressed] :styles.buttonInnerContainer}>
+                <Text style={styles.buttonText}>{children}</Text>
+            </Pressable>
         </View>
     )
 }
+
+const styles = StyleSheet.create({
+    buttonOuterContainer: {
+        borderRadius: 28,
+        margin: 4,
+        overflow: `hidden`
+    },
+    buttonInnerContainer: {
+        backgroundColor: `#4e0329`,
+        paddingVertical: 8,
+        paddingHorizontal: 16,
+        elevation: 4,
+
+    },
+
+    buttonText: {
+        color: `white`,
+        textAlign: `center`
+    },
+    pressed: {
+        opacity: 0.75
+    }
+})
 
 export default PrimaryButton
--- screens/StartScreen.js
+++ screens/StartScreen.js
@@ -1,11 +1,11 @@
-import { View, Text, TextInput, StyleSheet } from 'react-native'
+import { View, Text, TextInput, StyleShee, } from 'react-native'
 import React from 'react'
 import PrimaryButton from '../components/PrimaryButton.'
 
 const StartScreen = () => {
     return (
         <View style={styles.inputContainer}>
-            <TextInput />
+            <TextInput style={styles.numberInput} maxLength={2} keyboardType='number-pad' autoCorrect={false}/>
             <PrimaryButton>Reset</PrimaryButton>
             <PrimaryButton>Confirm</PrimaryButton>
         </View>
@@ -18,9 +18,25 @@ const styles = StyleSheet.create({
     inputContainer: {
         marginHorizontal: 16,
         borderRadius: 8,
-
         padding: 16,
         marginTop: 100,
-        backgroundColor: `#72063c`
+        backgroundColor: `#72063c`,
+        elevation: 6,
+        shadowOffset: 4,
+        shadowColor: `black`,
+        shadowOffset: { width: 2, height: 2 }
+    },
+
+    numberInput: {
+        height: 50,
+        fontSize: 32,
+        borderBottomColor: `#ddb52f`,
+        borderBottomWidth: 2,
+        color: `#ddb52f`,
+        marginVertical: 8,
+        fontWeight: `bold`,
+        width: 50,
+        textAlign: `center`
+
     }
 })
