8727412721f4d191c36dd156b7c3997468f6944b
unknown <diyanpetrov61@gmail.com>
2023-12-02 15:05:58 +0200

    styling fixes

--- App.js
+++ App.js
@@ -1,12 +1,17 @@
 import { StatusBar } from 'expo-status-bar';
+import { LinearGradient } from 'expo-linear-gradient';
 import { StyleSheet, Text, View } from 'react-native';
 import StartScreen from './screens/StartScreen';
 export default function App() {
   return (
-    <StartScreen />
+    <LinearGradient colors={[`#ddb52f`, `#72063c`]} style={styles.rootScreen} >
+      <StartScreen />
+    </LinearGradient>
   );
 }
 
 const styles = StyleSheet.create({
-
+  rootScreen: {
+    flex: 1
+  }
 });
--- screens/StartScreen.js
+++ screens/StartScreen.js
@@ -1,21 +1,34 @@
-import { View, Text, TextInput, StyleShee, } from 'react-native'
+import { View, Text, TextInput, StyleSheet, } from 'react-native'
 import React from 'react'
 import PrimaryButton from '../components/PrimaryButton.'
 
 const StartScreen = () => {
     return (
         <View style={styles.inputContainer}>
-            <TextInput style={styles.numberInput} maxLength={2} keyboardType='number-pad' autoCorrect={false}/>
-            <PrimaryButton>Reset</PrimaryButton>
-            <PrimaryButton>Confirm</PrimaryButton>
+            <TextInput style={styles.numberInput} maxLength={2} keyboardType='number-pad' autoCorrect={false} />
+            <View style={styles.buttonsContainer}>
+                <View style={styles.buttonContainer}>
+                    <PrimaryButton >Reset</PrimaryButton>
+                </View>
+                <View style={styles.buttonContainer}>
+                    <PrimaryButton >Confirm</PrimaryButton>
+                </View>
+            </View>
         </View>
     )
+
 }
 
 export default StartScreen
 
 const styles = StyleSheet.create({
+    buttonsContainer: {
+
+        flexDirection: `row`
+    },
     inputContainer: {
+        justifyContent: `center`,
+        alignItems: `center`,
         marginHorizontal: 16,
         borderRadius: 8,
         padding: 16,
@@ -25,6 +38,9 @@ const styles = StyleSheet.create({
         shadowOffset: 4,
         shadowColor: `black`,
         shadowOffset: { width: 2, height: 2 }
+    },
+    buttonContainer: {
+        flex: 1
     },
 
     numberInput: {
